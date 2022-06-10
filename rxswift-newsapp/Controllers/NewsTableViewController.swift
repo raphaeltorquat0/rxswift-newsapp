204eeb12e45441cca9a52030958cca0c4872dd45
Raphael Torquato <89878688+raphaeltorquat0@users.noreply.github.com>
2022-06-09 21:19:11 -0300

    Finish Project
    Finish Project

--- rxswift-newsapp/Controllers/NewsTableViewController.swift
+++ rxswift-newsapp/Controllers/NewsTableViewController.swift
@@ -14,20 +14,33 @@ class NewsTableViewController: UITableViewController {
         populateNews()
     }
     
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.articles.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleTableViewCell", for: indexPath) as? ArticleTableViewCell else {
+            fatalError("ArticleTableViewCell does not exists")
+        }
+        cell.titleLabel.text = self.articles[indexPath.row].title
+        cell.descriptionLabel.text = self.articles[indexPath.row].description
+        return cell
+    }
+    
     private func populateNews() {
-        let url = URL(string: "https://newsapi.org/v2/top-headlines?country=us&apiKey=56c9ef3908f540898aa5af0fdecb99ff")!
-        Observable.just(url).flatMap{ url -> Observable<Data> in
-            let request = URLRequest(url: url)
-            return URLSession.shared.rx.data(request: request)
-        }.map { data -> [Article]? in
-            return try? JSONDecoder().decode(ArticlesList.self, from: data).articles
-        }.subscribe(onNext: { [weak self] articles in
-            if let articles = articles {
-                self?.articles = articles
+        URLRequest.load(resource: ArticlesList.all).subscribe(onNext: { [weak self ] result in
+            if let result = result {
+                self?.articles = result.articles
                 DispatchQueue.main.async {
                     self?.tableView.reloadData()
+                
                 }
             }
+                
         }).disposed(by: disposeBag)
     }
 }
--- rxswift-newsapp/Models/Article.swift
+++ rxswift-newsapp/Models/Article.swift
@@ -8,3 +8,10 @@ struct Article: Decodable {
     let title: String
     let description: String
 }
+
+extension ArticlesList {
+    static var all: Resource<ArticlesList> = {
+       let url = URL(string: "https://newsapi.org/v2/top-headlines?country=us&apiKey=56c9ef3908f540898aa5af0fdecb99ff")!
+       return Resource(url: url)
+    }()
+}
